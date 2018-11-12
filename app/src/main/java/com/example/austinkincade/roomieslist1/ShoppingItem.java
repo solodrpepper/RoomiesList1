17dd77a2d44c93d6828acdcc35b9f578b0682560
Christopher Hawkins <christopherhawkins1996@gmail.com>
2018-11-12 14:54:22 -0700

    Added all the main class variables and functions for the user, list and item.

---
+++ app/src/main/java/com/example/austinkincade/roomieslist1/ShoppingItem.java
@@ -0,0 +1,35 @@
+package com.example.austinkincade.roomieslist1;
+
+import java.util.List;
+
+public class ShoppingItem {
+
+    private String itemName;
+    private float price;
+    private Boolean isChecked;
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Boolean getChecked() {
+        return isChecked;
+    }
+
+    public void setChecked(Boolean checked) {
+        isChecked = checked;
+    }
+
+}
---
+++ app/src/main/java/com/example/austinkincade/roomieslist1/ShoppingList.java
@@ -0,0 +1,49 @@
+package com.example.austinkincade.roomieslist1;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class ShoppingList {
+
+    private List<ShoppingItem> shoppingList;
+    private String originalCreatorID;
+    private String shoppingListName;
+
+    public List<ShoppingItem> getShoppingList() {
+        return shoppingList;
+    }
+
+    public void setShoppingList(List<ShoppingItem> shoppingList) {
+        shoppingList = shoppingList;
+    }
+
+    public String getOriginalCreatorID() {
+        return originalCreatorID;
+    }
+
+    public void setOriginalCreatorID(String originalCreatorID) {
+        this.originalCreatorID = originalCreatorID;
+    }
+
+    public String getShoppingListName() {
+        return shoppingListName;
+    }
+
+    public void setShoppingListName(String shoppingListName) {
+        this.shoppingListName = shoppingListName;
+    }
+
+    public void addShoppingItem(ShoppingItem item){
+        shoppingList.add(item);
+    }
+
+    public void removeShoppingItem(ShoppingItem item){
+        for (Iterator<ShoppingItem> iter = shoppingList.listIterator(); iter.hasNext(); ) {
+           ShoppingItem a = iter.next();
+            if (item.getItemName() == a.getItemName()) {
+                iter.remove();
+            }
+        }
+    }
+
+}
